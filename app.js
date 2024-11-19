287c31bc6f90e535ff75a95d23919766fee4bc90
Maheesha0720 <maheeshamaduwanthi2002@gmail.com>
2024-11-19 19:54:34 +0530

    second commit

--- app.js
+++ app.js
@@ -1,33 +1,41 @@
 let randomNum = Math.floor(Math.random() * 10 + 1);
-console.log(randomNum);
+
 let index = 0;
 let btnguess = document.getElementById("btnguess");
-let number = document.getElementById("number").value;
 let txtHint = document.getElementById("text-hint");
 let txtRound = document.getElementById("last-text");
-let btnNewGame= document.getElementById("btnNew");
-btnNewGame.disabled=true;
+let btnNewGame = document.getElementById("btnNew");
+btnNewGame.disabled = true;
 function guessNum() {
+  let number = document.getElementById("number").value;
+  
   if (number > randomNum) {
     index++;
-    txtHint.innerHTML = " guess a lesser one ..  ";
+    round(index);
+    txtHint.innerHTML = " guess a lower one ..  ";
   } else if (number < randomNum) {
     index++;
-    txtHint.innerHTML = " Guess a larger one ..  ";
+    round(index);
+    txtHint.innerHTML = " Guess a higher one ..  ";
   } else {
     index++;
     txtHint.innerHTML = "Congratulations..! You won the Game...  ";
+    txtRound.innerHTML = " ";
+    btnNewGame.disabled = false;
+    btnguess.disabled = true;
   }
   console.log(index);
-  if (index == 3) {
-    txtRound.innerHTML = "You have No more Chances..!!";
-    btnguess.disabled = true;
-    btnNewGame.disabled=false;
-  } else if (index < 3) {
-    txtRound.innerHTML = "You have " + (3 - index) + " more Chances..!!";
-  }
 }
 
 function reload() {
   location.reload();
+}
+function round(index) {
+  if (index == 3) {
+    txtRound.innerHTML = "Game over..You have No more Chances..!!";
+    btnguess.disabled = true;
+    btnNewGame.disabled = false;
+  } else if (index < 3) {
+    txtRound.innerHTML = "You have " + (3 - index) + " more Chances..!!";
+  }
 }
